1915c13685caa31a3519f2efd836f22055cd70de
Preeti Patil <preeti@setinfotech.com>
2018-06-07 17:38:22 +0530

    First Commit

---
+++ LU-Teacher/LU-Teacher/LUHeader/LUConstants.h
@@ -0,0 +1,16 @@
+//
+//  LUConstants.h
+//  LUTeacher
+//
+//  Created by Preeti Patil on 06/06/18.
+//  Copyright © 2018 Set Infotech. All rights reserved.
+//
+
+#ifndef LUConstants_h
+#define LUConstants_h
+
+
+#endif /* LUConstants_h */
+
+
+extern NSString *const kLUStudentRole;
---
+++ LU-Teacher/LU-Teacher/LUHeader/LUConstants.m
@@ -0,0 +1,12 @@
+//
+//  LUConstants.m
+//  LUTeacher
+//
+//  Created by Preeti Patil on 06/06/18.
+//  Copyright © 2018 Set Infotech. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "LUConstants.h"
+
+NSString *const kLUStudentRole  =  @"student";
